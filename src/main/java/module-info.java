ad7891aedbbb2dd6ca421c9aa52831b0cdaac9c9
simonkijo <simonkijo@gmail.com>
2023-03-10 19:23:05 +0300

    FRUITS MARKET APP

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.simonkijo.fruitsmarket {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.simonkijo.fruitsmarket to javafx.fxml;
+    exports com.simonkijo.fruitsmarket;
+}
