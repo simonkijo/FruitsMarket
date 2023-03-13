1976fda2b651513fc9de7769ab4a58bdeacf4471
simonkijo <simonkijo@gmail.com>
2023-03-13 13:13:30 +0300

    right bar view

--- src/main/java/com/simonkijo/fruitsmarket/MarketMain.java
+++ src/main/java/com/simonkijo/fruitsmarket/MarketMain.java
@@ -11,8 +11,8 @@ public class MarketMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MarketMain.class.getResource("market-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Fruits Market");
         stage.setScene(scene);
         stage.show();
     }
